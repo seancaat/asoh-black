e01c2bbfba88d5286a70412d8b2d9b4da88731df
Omayeli Arenyeka <oarenyeka@linkedin.com>
2019-08-29 11:48:17 -0700

    Get updates functionality

--- docs/assets/menu-693f79f0142b8fd4aaf1178d980e7990b5fecd55737f903645f136cd831088b8.js
+++
@@ -1,9 +0,0 @@
-function toggleMenu() {
-  document.querySelector('.menu').classList.toggle('hidden');
-  document.querySelector('.menu-button.close').classList.toggle('hidden');
-  document.querySelector('.menu-button.open').classList.toggle('hidden');
-}
-
-window.addEventListener('DOMContentLoaded', function() {
-  document.querySelector('.menu-controls').addEventListener('click', toggleMenu);
-});
